b9aa41a5fd50de3e532cd14480d141fe1b0c2ba9
Manny <104459715+emanuelbaca22@users.noreply.github.com>
2022-07-18 19:06:39 -0700

    Add files via upload

--- Message.java
+++ Message.java
@@ -1,1 +1,35 @@
-
+// Message Object
+public class Message {
+	// list properties based 
+	String from;
+	String to;
+	int msgSize;
+	String data;
+	String type;
+	
+	// methods of message class
+	public Message() {
+		// set Message object attributes here
+		
+	}
+	// list all getter methods
+	public String getFrom() {
+		return this.from;
+	}
+	
+	public String getTo() {
+		return this.to;
+	}
+	
+	public int getMsgSize() {
+		return this.msgSize;
+	}
+	
+	public String getData() {
+		return this.data;
+	}
+	
+	public String getType() {
+		return this.type;
+	}
+}
