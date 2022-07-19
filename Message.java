658b32ff85c051d0eec5063fab6df4e58b80a492
Manny <104459715+emanuelbaca22@users.noreply.github.com>
2022-07-18 19:18:37 -0700

    Add files via upload

--- Message.java
+++ Message.java
@@ -1,17 +1,27 @@
+import java.io.Serializable;
 // Message Object
-public class Message {
+public class Message implements Serializable{
 	// list properties based 
-	String from;
-	String to;
-	int msgSize;
-	String data;
-	String type;
+	private String from;
+	private String to;
+	private int msgSize;
+	private String data;
+	private String type;
 	
 	// methods of message class
-	public Message() {
+	public Message(String clientFrom, String clientTo, String clientData) {
 	// set Message object attributes here
+	this.from = clientFrom;
+	this.to = clientTo;
+	this.data = clientData;
+	// implement msg size here
+	
+	// Use a function to check whether the recipient is offline or online in order to set the message type
+	//this.type = checkType();
 		
 	}
+	// implement checkEmployeeStatus()
+	
 	// list all getter methods
 	public String getFrom() {
 		return this.from;
